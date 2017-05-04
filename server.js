4b6d753ac5ee5833140fe1b1ffebe82a464ccc80
Angelo Bovino <angelo.bovino@outlook.com>
2017-05-04 19:19:24 -0400

    hope there's no merge conflicts :fingers crossed:

---
+++ server.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('sample html');
+});
+
+app.listen(9000, () => {
+  console.log('Listening on port 9000');
+});
