cfd4ffe50ddb796f7a0ba12148584fe3869b84d0
Patrick Wu <scorpio7500@gmail.com>
2017-05-04 19:26:58 -0400

    add server things

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import DNS from 'dns'
+
+const app = express()
+const PORT = process.env.NODE_ENV || 8080
+
+
+// bless this function
+app.use(express.static('public/views'))
+app.use(express.static('public'))
+
+// // get private ip addr
+DNS.lookup(require('os').hostname(), (err, add, fam) => {
+    let APP_PRIVATE_IP_ADDRESS
+    APP_PRIVATE_IP_ADDRESS = add
+    app.listen(PORT, () => {
+        console.log('Listening on port', PORT, 'at private address', APP_PRIVATE_IP_ADDRESS)
+    })
+})
